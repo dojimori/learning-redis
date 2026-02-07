ea48c35e9db0289aa83236029f001ca4928d7437
dojimori <tuyor.jynn@gmail.com>
2026-02-07 17:16:00 +0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const axios = require('axios')
+const { createClient } = require('redis')
+
+const redis = createClient()
+
+redis.on('error', (err) => console.log('Error connecting to Redis', err))
+
+redis.connect().then(() => console.log('Redis connected'))
+
+var app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/leaderboard', async (req, res) => {
+  try {
+    const { score, player } = req.body;
+    if (!score || !player) return res.status(400).json({ message: 'score and player required.' });
+
+    await redis.zAdd('leaderboard', { score: parseInt(score), value: player });
+
+    res.status(200).send('leaderboard updated')
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+})
+
+app.listen(3000, () => {
+  console.log(`http://localhost:3000`)
+})
