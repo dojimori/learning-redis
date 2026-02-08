84abf87e948b904b893453ab14847c1b117091cf
dojimori <tuyor.jynn@gmail.com>
2026-02-08 09:19:10 +0800

    feat: add route for testing/experiment purposes

--- src/index.js
+++ src/index.js
@@ -16,6 +16,35 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/leaderboard', sortedSetsRoute);
 app.use('/hashes', hashesRoute);
 
+
+// ================ TEST ================ 
+
+app.post('/manga', async (req, res) => {
+  try {
+    const { author, volumes, isFinished, rating } = req.body;
+    const generatedId = crypto.randomUUID();
+    const key = `manga:${generatedId}`
+
+    // store the manga's data in hash
+    await redis.hSet(key, {
+      author,
+      volumes,
+      isFinished
+    });
+
+
+    // store the manga in a sorted set to sort by rating
+    await redis.zAdd('mangas', { score: rating, value: generatedId })
+
+    res.status(200).send('OK')
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+})
+
+
+
 app.listen(3000, () => {
   console.log(`http://localhost:3000`)
 })
